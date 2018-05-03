f5435485c48375ace690e5e066ebfbfb36481a74
Marie Curie Salera <mcsalera@gmail.com>
2018-05-03 08:45:17 +0800

    ogobs

---
+++ src/components/Login.vue
@@ -0,0 +1,26 @@
+<template>
+	<div class = "login">
+		<h3>Sign In</h3>
+		<input type="text" placeholder="Email"><br>
+		<input type="password" placeholder="Password"><br>
+		<button v-on:click="login">Connection</button>
+		<p>You don't have an account? You can <router-link to="/sign-up">create one</router-link></p>
+	</div>
+</template>
+
+<script>
+	export default{
+		name: 'login',
+		data: function(){
+			return {}
+		},
+	methods: {
+		login: function(){
+			this.$router.replace('hello')
+		}
+	}
+	}
+</script>
+
+<style scoped>
+</style>
---
+++ src/router/index.js
@@ -0,0 +1,29 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import HelloWorld from '@/components/HelloWorld'
+import Login from '@/components/Login'
+import SignUp from '@/components/SignUp'
+import Hello from '@/components/Hello'
+
+
+Vue.use(Router)
+
+export default new Router({
+  routes: [
+    {
+      path: '/',
+      name: 'HelloWorld',
+      component: HelloWorld
+    },
+    {
+      path: '/login',
+      name: 'Login',
+      component: Login
+    },
+    {
+      path: '/hello',
+      name: 'Hello',
+      component: Hello
+    }
+  ]
+})
